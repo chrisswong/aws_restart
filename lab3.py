0c9cb0e4a7535db375e607dccbd0018d70aedf63
chrisswong <chriswong.broadband@gmail.com>
2024-03-06 14:57:54 +0000

    Add files via upload
    init project

---
+++ lab3.py
@@ -0,0 +1,18 @@
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+myFruitList[2] = "orange"
+print(myFruitList)
+
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+
+myMixedTypeList = [45, 290578, 1.02, True, "My dog is on the bed.", "45"]
+
+for item in myMixedTypeList:
+    print("{} is of the data type {}".format(item,type(item)))
